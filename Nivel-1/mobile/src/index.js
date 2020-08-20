a3c5df2c435bd3e9a94d7f33d5a3f62ed3a14f0f
Diogo Sena <diogodmsena@gmail.com>
2020-08-19 23:31:39 -0300

    React-Native Concepts on First Aplication

--- Nivel-1/mobile/src/index.js
+++ Nivel-1/mobile/src/index.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+  
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function hendleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Diogo Sena'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
-      <StatusBar barStyle="light-content" />
-      <View style={styles.container}>
-        <Text style={styles.title}> Hello World!</Text>
-      </View>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+
+      <SafeAreaView style={styles.container}>
+        <FlatList           
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+        <TouchableOpacity 
+          activeOpacity={0.5} 
+          onPress={hendleAddProject} 
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+
+      {/* <View style={styles.container}>
+          {projects.map(project => (
+            <Text key={project.id} style={styles.project}>{project.title}</Text>
+          ))}
+      </View> */}
     </>
   );
 }
@@ -15,14 +57,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundcolor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#7159c1',
+    /*a scrollView não aceita */
+    //justifyContent: 'center',
+    //alignItems: 'center'
   },
 
-  title: {
+  project: {
     color: "#FFF",
-    fontSize: 32,
-    fontWeigth: 'bold'
+    fontSize: 20,
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+
   },
 });
