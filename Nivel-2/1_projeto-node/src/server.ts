81438adcc12dc0cccaa2b0176349687fd6940821
Diogo Sena <diogodmsena@gmail.com>
2020-08-25 23:52:51 -0300

    level 2: design patterns setup

--- Nivel-2/1_projeto-node/src/server.ts
+++ Nivel-2/1_projeto-node/src/server.ts
@@ -1,11 +1,11 @@
 import express, { request, response } from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello David!"})
-});
+app.get('/', (request, response) => response.json({ message: 'Hello David!' }));
 
 app.listen(3333, () => {
-  console.log("The server is running on port 3333!");
+  console.log('The server is running on port 3333!');
 });
