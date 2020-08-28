eb47464ac54960eb408c96542dc6e7f22fa8d35f
Diogo Sena <diogodmsena@gmail.com>
2020-08-28 12:36:46 -0300

    Implements Create Appointment Service

--- Nivel-2/1_projeto-node/src/server.ts
+++ Nivel-2/1_projeto-node/src/server.ts
@@ -1,10 +1,10 @@
 import express, { request, response } from 'express';
-
 import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello David!' }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('The server is running on port 3333!');
